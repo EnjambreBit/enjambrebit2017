5d0eccd49a22636cce8748ff20ff986b2b4624ad
digitalh <digital.h@gmail.com>
2017-07-19 18:08:40 -0300

    Agregando cabeceras de secciones. Empezando a implementar galeria para portfolio.

---
+++ app/components/ebit-galeria.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    didInsertElement(){
+    var $container = $('.lightbox-wrapper .items');
+    $container.imagesLoaded(function () {
+        $container.isotope({
+            itemSelector: '.lightbox-wrapper .item',
+            layoutMode: 'fitRows'
+        });
+    });
+    $('.lightbox-wrapper .filter li a').click(function () {
+
+        $('.lightbox-wrapper .filter li a').removeClass('active');
+
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector
+        });
+        return false;
+    });
+    }
+});
