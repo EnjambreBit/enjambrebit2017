2dabd1d6689deae4096276295fd686921ccd6499
Ivan Hoffmann <digital.h@gmail.com>
2017-07-31 17:25:02 -0300

    Instalando ember-responsive y empezando a implementar

--- app/components/ebit-navbar.js
+++ app/components/ebit-navbar.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: ""
+  tagName: "",
+  classNames: ["ui", "fixed", "menu", "inverted", "ebit-navbar"],
+  actions: {
+    toggle: function(id) {
+      $(`#${id}`).sidebar("toggle");
+    }
+  }
 });
